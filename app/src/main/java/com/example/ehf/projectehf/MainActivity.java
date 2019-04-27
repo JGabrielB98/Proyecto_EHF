20a2da057f3d616eb1afdf1dc0dae6af3c4ded42
Jorge Gabriel B <jorgem98gb@gmail.com>
2019-04-27 10:03:42 -0500

    Prueba 1

--- app/src/main/java/com/example/ehf/projectehf/MainActivity.java
+++ app/src/main/java/com/example/ehf/projectehf/MainActivity.java
@@ -9,5 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Haciendo una pequeña prueba de commit
     }
 }
