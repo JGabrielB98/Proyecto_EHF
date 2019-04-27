fda7c1a6c89830316ade11f97c00f76a01deb1ed
Jorge Gabriel B <jorgem98gb@gmail.com>
2019-04-27 10:10:36 -0500

    Prueba 2

--- app/src/main/java/com/example/ehf/projectehf/MainActivity.java
+++ app/src/main/java/com/example/ehf/projectehf/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Haciendo una pequeña prueba de commit
+        // Segunda prueba
     }
 }
